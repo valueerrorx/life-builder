e2b60a2d11ab7e3c7d43e2e9004d79c1526ef333
valueerror <you@example.com>
2018-01-08 09:32:30 +0100

    changed EFI files

--- conf/config.py
+++ conf/config.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 BASEWORKDIR='/home/lifebuilder'
-LIVEUSER='waldelf'
+LIVEUSER='student'
 LIVEHOSTNAME='life'
 CUSTOMISO='life-dist1.iso'
-SKELUSER='waldelf'
+SKELUSER='student'
 LIVECDLABEL='life'
 LIVECDURL='http://life-edu.eu'
 EXCLUDES='Nextcloud ownCloud'
 SQUASHFSOPTS='-no-recovery -always-use-fragments -b 1M -no-duplicates -comp xz'
 COPYSKEL='False'
 COPYDEFAULTUSER='True'
+REMOVERESTRICTED='False'
